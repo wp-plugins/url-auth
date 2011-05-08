c24eb4a97f02babaede62b58cdf1cacd4ff61424
chadparry <chadparry@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-08 15:02:50 +0000

    Importing v0.2 source
    git-svn-id: https://plugins.svn.wordpress.org/url-auth/trunk@382479 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- url-auth.php
+++ url-auth.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: URL auth
 Description: Allows a URL to contain login information
-Version: 0.1
+Version: 0.2
 Author: Chad Parry
 
 This work is derived from Post Restrictions by S&ouml;ren Weber (http://soeren-weber.net/post/2006/01/01/121/)
@@ -23,7 +23,7 @@ function enable_url_auth()
 		// I wish there were a more targeted way to override this
 		function get_currentuserinfo()
 		{
-			global $user_login, $userdata, $user_level, $user_ID, $user_email, $user_url, $user_pass_md5, $user_identity, $current_user;
+			global $current_user, $wp_version;
 
             		if (!empty($current_user))
                 		return;
@@ -32,14 +32,22 @@ function enable_url_auth()
 			$user = $login[0];
 			$password = $login[1];
 
-			// Use HTTP auth instead of cookies
-			if(!wp_login($user, $password))
-			{
-				header('HTTP/1.0 401 Unauthorized');
-				die(__('Incorrect credentials', 'url_auth'));
+			if (version_compare($wp_version, '3.0', '<')) {
+				if (!wp_login($user, $password))
+				{
+					header('HTTP/1.0 401 Unauthorized');
+					die(__('Incorrect credentials', 'url_auth'));
+				}
+				wp_set_current_user(null, $user);
+			} else {
+				if (is_wp_error(wp_signon(array('user_login' => $user, 'user_password' => $password))))
+				{
+					header('HTTP/1.0 401 Unauthorized');
+					die(__('Incorrect credentials', 'url_auth'));
+				}
+				$user_info = get_userdatabylogin($user);
+				set_current_user(null, $user_info->ID);
 			}
-
-			set_current_user(0, $user);
 
 		}
 	}
@@ -55,7 +63,7 @@ if (!function_exists('add_filter'))
   die(__("This page must be loaded as part of WordPress", 'url_auth'));
 
 global $wp_version;
-if (substr($wp_version, 0, 2) == "1.")
+if (version_compare($wp_version, '2.0', '<'))
   die(__("This plugin requires at least WordPress 2.0", 'url_auth'));
 
 enable_url_auth();
